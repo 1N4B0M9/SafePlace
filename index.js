b57ae62f0ce1083f448d3581bb6c82f980da4c81
Nathan Morelli <nathanmorelli@Nathans-MacBook-Pro.local>
2024-09-28 14:00:31 -0400

    mongo updates

--- index.js
+++ index.js
@@ -2,20 +2,33 @@ const { MongoClient } = require("mongodb");
 // Replace the uri string with your connection string.
 const uri = "mongodb+srv://2005nathan2005:NOiaiXkLH9FbKxyo@safespace.bbqvc.mongodb.net/?retryWrites=true&w=majority&appName=SafeSpace";
 const client = new MongoClient(uri);
+
 async function run() {
   try {
-    const database = client.db('sample_mflix');
-    const movies = database.collection('movies');
+    const database = client.db('UserData');
+    //const movies = database.collection('movies');
     // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-    console.log(movie);
+    //const query = { title: 'Back to the Future' };
+   //const movie = await movies.findOne(query);
+   // console.log(movie);
+    var myobj = { incident: "help", location: "Highway 37" };
+    database.collection("UserDataCollection").insertOne(myobj);
+
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
+
 run().catch(console.dir);
+
+function getInformation(incident, location){
+    this.incident = incident;
+    this.location = location;
+   
+
+}
+
 
 
 
