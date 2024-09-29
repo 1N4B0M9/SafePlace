3dacc544631787f9297713d2c482d830141a28f8
Nathan Morelli <nathanmorelli@Nathans-MacBook-Pro.local>
2024-09-28 23:18:23 -0400

    back to normal

--- index.js
+++ index.js
@@ -19,52 +19,6 @@ async function run() {
 
 run();
 
-// Create a server
-/*const server = http.createServer(async (req, res) => {
-  // Handle request for the root URL
-  if (req.url === '/') {
-    const filePath = path.join(__dirname, 'public', 'index.html');
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Error reading file.\n');
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      }
-
-    });
-  } else if (req.url === '/report' && req.method === 'POST') {
-    let body = '';
-
-    // Collect data from the request
-    req.on('data', chunk => {
-      body += chunk.toString(); // Convert Buffer to string
-    });
-
-    req.on('end', async () => {
-      try {
-        const reportData = JSON.parse(body);
-
-        // Insert the report into the MongoDB collection
-        const database = client.db('UserData');
-        await database.collection('UserDataCollection').insertOne(reportData);
-
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'Report submitted successfully!' }));
-      } catch (error) {
-        console.error(error);
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Error saving report.\n');
-      }
-    });
-  } else {
-    // Handle 404 for any other routes
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('404 Not Found\n');
-  }
-});
-*/
 const server = http.createServer(async (req, res) => {
   if (req.url === '/') {
     const filePath = path.join(__dirname, 'public', 'index.html');
@@ -126,6 +80,16 @@ const server = http.createServer(async (req, res) => {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Error saving report.\n');
       }
+    });
+  }  else if (req.url.endsWith('.css')) { // Handle CSS files
+    const filePath = path.join(__dirname, 'public', req.url);
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        return res.end('404 Not Found\n');
+      }
+      res.writeHead(200, { 'Content-Type': 'text/css' });
+      res.end(data);
     });
   } else {
     // Handle 404 for any other routes
